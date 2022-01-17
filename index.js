49595f8e9c704fe87ea6d426d38b2efef083078e
David Schepis <davidschepis@gmail.com>
2022-01-16 18:27:03 -0700

    updated code to use async functions

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const createNewDepartment = () => {
 };
 
 //this function handles the prompt for adding a new role
-const createNewRole = () => {
+const createNewRole = async () => {
     inquirer.prompt(
         [
             {
@@ -75,7 +75,7 @@ const createNewRole = () => {
                 type: "list",
                 message: "Which department does this role belong to?",
                 name: "department",
-                choices: getDepartmentNames()
+                choices: await getDepartmentNames()
             }
         ]
     ).then((response) => {
@@ -86,7 +86,7 @@ const createNewRole = () => {
 };
 
 //this function handles the prompt for adding a new employee
-const createNewEmployee = () => {
+const createNewEmployee = async () => {
     inquirer.prompt(
         [
             {
@@ -103,13 +103,13 @@ const createNewEmployee = () => {
                 type: "list",
                 message: "Enter the employee's role",
                 name: "role",
-                choices: getEmployeeRoles()
+                choices: await getEmployeeRoles()
             },
             {
                 type: "list",
                 message: "Enter the employee's manager",
                 name: "manager",
-                choices: getManagerNames()
+                choices: await getManagerNames()
             }
         ]
     ).then((response) => {
@@ -120,20 +120,20 @@ const createNewEmployee = () => {
 };
 
 //this function handles the prompt for updating an employee's role
-const updateEmployeeRole = () => {
+const updateEmployeeRole = async () => {
     inquirer.prompt(
         [
             {
                 type: "list",
                 message: "Which employee to update?",
                 name: "employee",
-                choices: getEmployeeNames()
+                choices: await getEmployeeNames()
             },
             {
                 type: "list",
                 message: "What is the employee's new role?",
                 name: "role",
-                choices: getEmployeeRoles()
+                choices: await getEmployeeRoles()
             }
         ]
     ).then((response) => {
@@ -169,109 +169,125 @@ const handleResponse = (response) => {
 //handles the sql commands to add a new department
 const addDepartmentToDB = (name) => {
     db.promise().query(`insert into department (name) values ("${name}")`)
-    .then((response) => {
-        console.info(`${name} department added to the db`);
-        getUserInput();
-    })
+        .then((response) => {
+            console.info(`${name} department added to the db`);
+            getUserInput();
+        })
 };
 
 //uses sql to return all department names from the database in an array
-const getDepartmentNames = () => {
-    let names = [];
+const getDepartmentNames = async () => {
+    return new Promise(resolve => {
+        let names = [];
     db.promise().query(`select * from department`)
-    .then((response) => {
-        for (let i = 0; i < response[0].length; i++) {
-            names.push(response[0][i].name);
-        }
-    }).catch(console.log);
-    return names;
+        .then((response) => {
+            for (let i = 0; i < response[0].length; i++) {
+                names.push(response[0][i].name);
+            }
+        }).catch(console.log);
+    resolve(names);
+    });
 };
 
 //adds a new role to the role table
 const addNewRoleToDB = (title, salary, department) => {
     db.promise().query(`select department.id from department where department.name = "${department}"`)
-    .then((response) => {
-        department = response[0][0].id;
-        db.promise().query(`insert into role (title, salary, department_id) values ("${title}", "${salary}", "${department}")`).then((response) => {
-            console.info(`${title} role with ${salary} salary added to the db`);
-            getUserInput();
+        .then((response) => {
+            department = response[0][0].id;
+            db.promise().query(`insert into role (title, salary, department_id) values ("${title}", "${salary}", "${department}")`).then((response) => {
+                console.info(`${title} role with ${salary} salary added to the db`);
+                getUserInput();
+            });
         });
-    });
 };
 
 //adds a new employee to the employee table
 const addNewEmployeeToDB = (firstName, lastName, role, manager) => {
     const managerDetails = manager.split(" ");
     db.promise().query(`select employee.id from employee where employee.first_name="${managerDetails[0]}" and employee.last_name="${managerDetails[1]}"`)
-    .then((response) => {
-        let id = 0;
-        if (manager !== "None") {
-            id = response[0][0].id;
-        }
-        db.promise().query(`select role.id from role where role.title="${role}"`)
         .then((response) => {
-            let roleID = response[0][0].id;
-            db.promise().query(`insert into employee (first_name, last_name, role_id, manager_id) values ("${firstName}", "${lastName}", "${roleID}", "${id}")`)
-            .then((response) => {
-            console.info(`${firstName} ${lastName} RoleID: ${roleID} ManagerID: ${id} added to the db`);
-            getUserInput();
-            });
+            let id = 0;
+            if (manager !== "None") {
+                id = response[0][0].id;
+            }
+            db.promise().query(`select role.id from role where role.title="${role}"`)
+                .then((response) => {
+                    let roleID = response[0][0].id;
+                    db.promise().query(`insert into employee (first_name, last_name, role_id, manager_id) values ("${firstName}", "${lastName}", "${roleID}", "${id}")`)
+                        .then((response) => {
+                            console.info(`${firstName} ${lastName} RoleID: ${roleID} ManagerID: ${id} added to the db`);
+                            getUserInput();
+                        });
+                });
         });
-    });
 };
 
 //returns all roles as an array
 const getEmployeeRoles = () => {
-    let roles = [];
-    db.promise().query(`select * from role`)
-    .then((response) => {
-        for (let i = 0; i < response[0].length; i++) {
-            roles.push(response[0][i].title);
-        }
-    }).catch(console.log);
-    return roles;
+    return new Promise(resolve => {
+        let roles = [];
+        db.promise().query(`select * from role`)
+            .then((response) => {
+                for (let i = 0; i < response[0].length; i++) {
+                    roles.push(response[0][i].title);
+                }
+            }).catch(console.log);
+        resolve(roles);
+    });
 };
 
 //returns an array of all the managers
 const getManagerNames = () => {
-    let names = [];
-    names.push("None");
-    db.promise().query(`select distinct b.first_name, b.last_name from employee as a, employee as b where a.manager_id = b.id`)
-    .then((response) => {
-        for (let i = 0; i < response[0].length; i++) {
-            names.push(response[0][i].first_name + " " + response[0][i].last_name);
-        }
-    }).catch(console.log);
-    return names;
+    return new Promise(resolve => {
+        let names = [];
+        names.push("None");
+        db.promise().query(`select distinct b.first_name, b.last_name from employee as a, employee as b where a.manager_id = b.id`)
+            .then((response) => {
+                for (let i = 0; i < response[0].length; i++) {
+                    names.push(response[0][i].first_name + " " + response[0][i].last_name);
+                }
+                resolve(names);
+            }).catch(console.log);
+    });
 };
 
-// const getEmployeeNames = () => {
-
-// };
+//returns all employees's names
+const getEmployeeNames = () => {
+    return new Promise(resolve => {
+        db.promise().query("select * from employee")
+            .then((response) => {
+                let names = [];
+                for (let i = 0; i < response[0].length; i++) {
+                    names.push(response[0][i].first_name + " " + response[0][i].last_name);
+                }
+                resolve(names);
+            })
+    })
+};
 
 //displays all departments in table
 const showDepartmentNames = async () => {
     await db.promise().query(`select * from department`)
-    .then((response) => {
-        console.table(response[0]);
-    }).catch(console.log);
+        .then((response) => {
+            console.table(response[0]);
+        }).catch(console.log);
     getUserInput();
 };
 
 //displays all roles in table
 const showRoles = async () => {
     await db.promise().query(`select * from role`)
-    .then((response) => {
-        console.table(response[0]);
-    }).catch(console.log);
+        .then((response) => {
+            console.table(response[0]);
+        }).catch(console.log);
     getUserInput();
 };
 
 const showEmployees = async () => {
     await db.promise().query(`select * from employee`)
-    .then((response) => {
-        console.table(response[0]);
-    }).catch(console.log);
+        .then((response) => {
+            console.table(response[0]);
+        }).catch(console.log);
     getUserInput();
 };
 
